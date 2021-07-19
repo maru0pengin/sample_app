cace172715658fb9af5c99f3242e2f5d93ff6394
maru0pengin <maru0pengin@gmail.com>
2021-07-19 21:16:47 +0900

    7.4まで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
-      #redirect_to user_url(@user)
+      #redirect_to user_url(@user) 同じ
     else
       render 'new'
     end
